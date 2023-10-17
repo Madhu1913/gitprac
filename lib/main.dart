5a9c7a2eda53161a3edbb09d0106aaa524110455
HP <madhunarayanaedu1913@gmail.com>
2023-10-17 21:32:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: FirstScreen(),debugShowCheckedModeBanner: false,);
+  }
+}
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Row(
+          children: [
+            Text('First'),
+            Text('Second'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
